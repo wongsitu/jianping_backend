773fb746f9584be9bfaeac7d0146dd2190fa7ecb
wongsitu <sa_waika_4d_23@hotmail.com>
2022-03-06 00:58:59 -0500

    envs

--- jianping_backend/settings.py
+++ jianping_backend/settings.py
@@ -96,6 +96,7 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 AWS_DEFAULT_ACL = 'public-read'
+AWS_LOCATION = 'static'
 
 if(DEBUG is True):
     AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
