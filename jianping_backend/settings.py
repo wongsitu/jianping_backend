510f0ab07f253cf92bd78e64bc679e3370675714
wongsitu <sa_waika_4d_23@hotmail.com>
2022-03-05 23:41:39 -0500

    digital ocean spaces

--- jianping_backend/settings.py
+++ jianping_backend/settings.py
@@ -98,7 +98,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 if(DEBUG is True):
     AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 else:
-    AWS_S3_CUSTOM_DOMAIN = '%s.ams3.cdn.digitaloceanspaces.com' % AWS_STORAGE_BUCKET_NAME
+    AWS_S3_CUSTOM_DOMAIN = '%s.nyc3.cdn.digitaloceanspaces.com' % AWS_STORAGE_BUCKET_NAME
 
 AWS_LOCATION = 'static'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
