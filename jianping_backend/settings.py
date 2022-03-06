50bc17eab82b31ad0a04da5a6dea61d4e82554bb
wongsitu <sa_waika_4d_23@hotmail.com>
2022-03-06 01:37:21 -0500

    envs

--- jianping_backend/settings.py
+++ jianping_backend/settings.py
@@ -102,7 +102,7 @@ AWS_LOCATION = 'static'
 if(DEVELOPMENT_MODE is True):
     AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 else:
-    AWS_S3_CUSTOM_DOMAIN = '%s.nyc3.cdn.digitaloceanspaces.com' % AWS_STORAGE_BUCKET_NAME
+    AWS_S3_CUSTOM_DOMAIN = '%s.nyc3.digitaloceanspaces.com' % AWS_STORAGE_BUCKET_NAME
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
